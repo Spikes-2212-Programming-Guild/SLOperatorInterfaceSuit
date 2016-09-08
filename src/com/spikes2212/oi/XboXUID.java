29b581c456b0fa1d8a3d38136171d221c57dd578
Spikes 2212 <spikes.no2212@gmail.com>
2016-09-08 20:02:32 +0300

    first commit

---
+++ src/com/spikes2212/oi/XboXUID.java
@@ -0,0 +1,76 @@
+package com.spikes2212.oi;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class XboXUID extends Joystick {
+
+	
+	
+	public XboXUID(int port) {
+		super(port);
+	}
+
+	public Button getGreenButton() {
+		return new JoystickButton(this, 2);
+	}
+
+	public Button getBlueButton() {
+		return new JoystickButton(this, 1);
+	}
+
+	public Button getRedButton() {
+		return new JoystickButton(this, 3);
+	}
+
+	public Button getYellowButton() {
+		return new JoystickButton(this, 4);
+	}
+
+	public boolean getRawYellowButton() {
+		return this.getRawButton(4);
+	}
+
+	public boolean getRawBlueButton() {
+		return this.getRawButton(1);
+	}
+
+	public boolean getRawRedButton() {
+		return this.getRawButton(3);
+	}
+
+	public boolean getRawGreenButton() {
+		return this.getRawButton(2);
+	}
+
+	public Button getRtButton() {
+		return new JoystickButton(this, 8);
+	}
+	public Button getRbButton() {
+		return new JoystickButton(this, 6);
+	}
+	public Button getLtButton() {
+		return new JoystickButton(this, 7);
+	}
+	public Button getLbButton() {
+		return new JoystickButton(this, 5);
+	}
+	public boolean getRawRtButton() {
+		return this.getRawButton(8);
+	}
+	public boolean getRawRbButton() {
+		return this.getRawButton(6);
+	}
+	public boolean getRawLtButton() {
+		return this.getRawButton(7);
+	}
+	public boolean getRawLbButton() {
+		return this.getRawButton(5);
+	}
+	public Button getxutton() {
+		return new JoystickButton(this, 0);
+	}
+	
+}
+
